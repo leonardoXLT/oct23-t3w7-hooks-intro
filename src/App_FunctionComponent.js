f8f61fc276c649b91aa5deb448dfb64bdfaa8450
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-05-16 19:24:19 +1000

    complete version

--- src/App_FunctionComponent.js
+++ src/App_FunctionComponent.js
@@ -1,10 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
   const [message, setMessage] = useState("Hello world");
-  const [state2, setState2] = useState("e");
+  const [pokemon, setPokemon] = useState({name: "", sprite: ""});
+
+
+  // useEffect with empty dependency array = 
+  // depends on nothing, will run when declared
+  // but never again.
+  useEffect(() => {
+    console.log("Component loaded");
+  }, []);
+
+  // useEffect with return function = 
+  // gonna call that function on componentWillUnmount
+  // not related to the dependency array at all 
+  useEffect(() => {
+    return (() => {
+      console.log("Component will unmount");
+    });
+  }, []);
+
+  // useEffect with variable in dependency array = 
+  // depends on variable, will run when declared AND updated 
+  // but not on anything not listed in dependency array 
+  useEffect(() => {
+    console.log("Message updated, new value is: " + message);
+  }, [message]);
+
+
+  const getPokemon = async () => {
+    let result = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 1025) + 1}`).then(response => response.json());
+    setPokemon({name: result.name, sprite: result.sprites.front_default});
+  }
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
 
   return (
     <div className="App">
@@ -16,6 +51,12 @@ function App() {
 
       <input type='text' value={message} onChange={(event) => setMessage(event.target.value)} />
 
+
+      <h1>{pokemon.name}</h1>
+      <img src={pokemon.sprite} alt='URL to the sprite of whatever random Pokemon this is.' />
+      <button onClick={getPokemon}>
+        Get random Pokemon 
+      </button>
     </div>
   );
 }
